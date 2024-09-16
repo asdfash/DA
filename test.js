71dc820a04cdc023828b782784737bc7f57fc7ec
Jit <88709514+asdfash@users.noreply.github.com>
2024-09-16 10:18:59 +0800

    bugfix

--- test.js
+++ test.js
@@ -7,26 +7,27 @@ const mysql = require("mysql2/promise");
 const argon2 = require("argon2");
 
 async function test() {
-  // // MYSQL query test
-  // const connection = await mysql.createConnection({
-  //   host: process.env.SQLHOST || 'localhost',
-  //   port: process.env.SQLPORT || 3306,
-  //   user: process.env.SQLUSER || 'root',
-  //   database: process.env.SQLDB || 'db',
-  //   password: process.env.SQLPW || 'P@ssw0rd123',
-  // });
-  // // SELECT query
-  // try {
-  //   const email = "test@test.com";
-  //   const pw = "test";
-  //   const [results, fields] = await connection.execute("SELECT `accounts` WHERE `email` = ? AND `password` = ?)", [email, pw]);
-  //   console.log("---------");
-  //   console.log(results); // results contains rows returned by server
-  //   console.log("---------");
-  //   console.log(fields); // fields contains extra meta data about results, if available
-  // } catch (err) {
-  //   console.log(err);
-  // }
+  const connection = await mysql.createConnection({
+    host: process.env.SQLHOST,
+    port: process.env.SQLPORT,
+    user: process.env.SQLUSER,
+    database: process.env.SQLDB,
+    password: process.env.SQLPW,
+  });
+
+  // SELECT query
+  try {
+    [];
+    const email = "test@test.com";
+    const pw = "test";
+    const [results, fields] = await connection.execute("SELECT * FROM `accounts` WHERE `email` = ? AND `password` = ?", [email, pw]);
+    console.log("---------");
+    console.log(results); // results contains rows returned by server
+    console.log("---------");
+    console.log(fields); // fields contains extra meta data about results, if available
+  } catch (err) {
+    console.log(err);
+  }
   /**
    * BREAKPOINT
    */
