ac3944cbe6c0b004e9ee3a404221152b293e7ee2
Jit <88709514+asdfash@users.noreply.github.com>
2024-09-16 10:30:33 +0800

    refresh

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const helmet = require("helmet");
 const xssClean = require("xss-clean");
 const hpp = require("hpp");
 const cors = require("cors");
+const apiController = require("./controllers/apiController");
 
 const app = express();
-const port = 3000;
+const port = process.env.BACKENDPORT || 3000;
 
 //rate limiter
 const limiter = rateLimit({
@@ -35,60 +36,12 @@ app.use(
 app.use(cors());
 
 // Inititalize the app and add middleware
-app.set("view engine", "pug"); // Setup the pug
 app.use(express.urlencoded({ extended: true })); // Setup the body parser to handle form submits
 app.use(session({ secret: "super-secret" })); // Session setup
 
-/** Handle login display and form submit */
-app.get("/login", (req, res) => {
-  if (req.session.isLoggedIn === true) {
-    return res.redirect("/");
-  }
-  res.render("login", { error: false });
-});
-
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  if (username === "bob" && password === "1234") {
-    req.session.isLoggedIn = true;
-    res.redirect(req.query.redirect_url ? req.query.redirect_url : "/");
-  } else {
-    res.render("login", { error: "Username or password is incorrect" });
-  }
-});
-
-/** Handle logout function */
-app.get("/logout", (req, res) => {
-  req.session.isLoggedIn = false;
-  res.redirect("/");
-});
-
-/** Simulated bank functionality */
-app.get("/", (req, res) => {
-  res.render("index", { isLoggedIn: req.session.isLoggedIn });
-});
-
-app.get("/balance", (req, res) => {
-  if (req.session.isLoggedIn === true) {
-    res.send("Your account balance is $1234.52");
-  } else {
-    res.redirect("/login?redirect_url=/balance");
-  }
-});
-
-app.get("/account", (req, res) => {
-  if (req.session.isLoggedIn === true) {
-    res.send("Your account number is ACL9D42294");
-  } else {
-    res.redirect("/login?redirect_url=/account");
-  }
-});
-
-app.get("/contact", (req, res) => {
-  res.send("Our address : 321 Main Street, Beverly Hills.");
-});
+apiController(app);
 
 /** App listening on port */
 app.listen(port, () => {
-  console.log(`MyBank app listening at http://localhost:${port}`);
+  console.log(`API listening on http://localhost:${port}`);
 });
